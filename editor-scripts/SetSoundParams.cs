a53828b2fce4d096e3830ec72b81315faf8285fc
amaeland <atle.maeland@gmail.com>
2014-04-09 09:57:38 +0200

    Check for changes before re-importing
    - we dont need to re-import assets that already have the wanted settings
    applied.

--- editor-scripts/SetSoundParams.cs
+++ editor-scripts/SetSoundParams.cs
@@ -1,20 +1,24 @@
 ﻿using UnityEditor;
 using UnityEngine;
 public class SetSoundParams : MonoBehaviour {
-
+	
 	[MenuItem ("Tools/Custom/Set sound params")]
 	static void DoSetSoundParams()
 	{
 		var objects = Selection.objects;
-
+		
 		foreach(Object obj in objects) {
 			string path = AssetDatabase.GetAssetPath(obj);
 			AudioImporter audioImporter = AssetImporter.GetAtPath(path) as AudioImporter;
-			//add your sound settings here, more info -> https://docs.unity3d.com/Documentation/ScriptReference/AudioImporter.html
-			audioImporter.threeD = false;
 
-			//re-import assets after you are done changing settings - without this the changes will be discarded!
-			AssetDatabase.ImportAsset(path, ImportAssetOptions.ForceUpdate | ImportAssetOptions.Default);
+			if(audioImporter.threeD != false)
+			{
+				//add your sound settings here, more info -> https://docs.unity3d.com/Documentation/ScriptReference/AudioImporter.html
+				audioImporter.threeD = false;
+
+				//re-import assets after you are done changing settings - without this the changes will be discarded!
+				AssetDatabase.ImportAsset(path, ImportAssetOptions.ForceUpdate | ImportAssetOptions.Default);
+			}
 		}
 	}
 }
